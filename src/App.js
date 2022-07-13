144b559d8d147f56fc518ff7b3d96de255e011c6
uf-code <ugur.firat.developer@gmail.com>
2022-07-13 16:19:39 +0300

    palindrome check

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import { Palindrome } from './components/palindrome'
+
 export const App = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+  return (
+    <div className='bg-slate-800 h-screen'>
+      <Palindrome />
+    </div>
+  )
 }
---
+++ src/components/palindrome.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+import { TextInput, Button } from 'flowbite-react'
+import { CheckIcon, XIcon } from '@heroicons/react/solid'
+
+export const Palindrome = () => {
+  const [palindrome, setPalindrome] = useState('')
+  const [isPal, setIsPalindrome] = useState(null)
+
+  const handleTextChange = (e) => setPalindrome(e.target.value)
+
+  const isPalindrome = (txt) => {
+    var re = /[\W_]/g
+    var lowRegStr = txt.toLowerCase().replace(re, '')
+    var reverseStr = lowRegStr.split('').reverse().join('')
+
+    setIsPalindrome(reverseStr === lowRegStr)
+  }
+
+  return (
+    <div className='flex justify-center pt-10'>
+      {/*  */}
+      <Button
+        className=''
+        onClick={() => {
+          console.log(palindrome)
+          console.log(isPalindrome(palindrome))
+        }}
+        outline={true}
+        gradientDuoTone='cyanToBlue'
+      >
+        Check If Palindrome
+      </Button>
+
+      <TextInput
+        className='dark:border-gray-500 dark:bg-gray-600'
+        value={palindrome}
+        onChange={handleTextChange}
+      />
+
+      {isPal === true ? (
+        <CheckIcon className='h-10 w-10 text-green-500' />
+      ) : (
+        <XIcon className='h-10 w-10 text-red-500' />
+      )}
+    </div>
+  )
+}
